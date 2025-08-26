026229b277590a950f4e934ffa9bdc255b152641
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-08-26 23:18:27 +0600

    add speed boosting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,19 +9,42 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float torqueAmount = 1f;
     [SerializeField] private float restartDelay = 1f;
     [SerializeField] private ParticleSystem crashEffect;
+    [SerializeField] private float baseSpeed = 12f;
+    [SerializeField] private float boostSpeed = 15f;
+    private Vector2 moveInput;
+    private SurfaceEffector2D surfaceEffector2D;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         rb = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        MovePlayer();
+        BoostPlayer();
+    }
+
+    private void MovePlayer()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
         rb.AddTorque(-moveInput.x * torqueAmount);
+    }
+
+    private void BoostPlayer()
+    {
+        if (moveInput.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
